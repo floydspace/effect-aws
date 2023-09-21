f7e9e3ed4c2cb5dc0e0b9e0895278307a182a0d2
Victor Korzunin <5180700+floydspace@users.noreply.github.com>
2023-09-21 17:17:02 +0200

    fix: correct way of propagating default logger instance inside S3Client

---
+++ .changeset/rich-cherries-try.md
@@ -0,0 +1,5 @@
+---
+"@effect-aws/client-s3": patch
+---
+
+correct way of propagating default logger instance inside S3Client, thanks to @mikearnaldi
--- packages/client-s3/src/Context.ts
+++ packages/client-s3/src/Context.ts
@@ -3,20 +3,26 @@ import * as Context from "@effect/data/Context";
 import { flow } from "@effect/data/Function";
 import * as Effect from "@effect/io/Effect";
 import * as Layer from "@effect/io/Layer";
+import * as Runtime from "@effect/io/Runtime";
 
 export const S3ClientConfigTag = Context.Tag<S3ClientConfig>(
   "@effect-aws/S3Client/Config",
 );
 
-export const DefaultS3ClientConfigLayer = Layer.succeed(S3ClientConfigTag, {
-  logger: {
-    info: flow(Effect.logInfo, Effect.runSync),
-    warn: flow(Effect.logWarning, Effect.runSync),
-    error: flow(Effect.logError, Effect.runSync),
-    debug: flow(Effect.logDebug, Effect.runSync),
-    trace: flow(Effect.logTrace, Effect.runSync),
-  },
-});
+export const DefaultS3ClientConfigLayer = Layer.effect(
+  S3ClientConfigTag,
+  Effect.runtime<never>().pipe(
+    Effect.map((runtime) => ({
+      logger: {
+        info: flow(Effect.logInfo, Runtime.runSync(runtime)),
+        warn: flow(Effect.logWarning, Runtime.runSync(runtime)),
+        error: flow(Effect.logError, Runtime.runSync(runtime)),
+        debug: flow(Effect.logDebug, Runtime.runSync(runtime)),
+        trace: flow(Effect.logTrace, Runtime.runSync(runtime)),
+      },
+    })),
+  ),
+);
 
 export const S3ClientInstanceTag = Context.Tag<S3Client>(
   "@effect-aws/S3Client/Instance",
